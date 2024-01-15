a127f53c2c0aaa2d06f0348fb17086e49b1a5918
Burhan Jamil Chaud-Ry <burhan.chaud-ry@student.ehb.be>
2024-01-15 19:52:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RequestQueue requestQueue;
+    private List<Movie> movieList;
+    private SearchView searchView;
+
+    private MovieAdapter movieAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        searchView = findViewById(R.id.searchView);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return true;
+            }
+        });
+
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        requestQueue = VolleySingleton.getmInstance(this).getRequestQueue();
+
+        movieList = new ArrayList<>();
+        fetchMovies();
+    }
+
+    private void filterList(String text) {
+        List<Movie> filteredList = new ArrayList<>();
+        for (Movie movie : movieList){
+            if (movie.getTitle().toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(movie);
+            }
+        }
+        if (filteredList.isEmpty()){
+            Toast.makeText(this, "No data found", Toast.LENGTH_SHORT).show();
+        }else {
+            movieAdapter.setFilteredList(filteredList);
+        }
+    }
+
+
+
+
+    private void fetchMovies() {
+
+        String url = "https://api.npoint.io/2ea4fcf39a806a4b0996";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        for (int i = 0 ; i < response.length() ; i ++){
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                String title = jsonObject.getString("title");
+                                String overview = jsonObject.getString("overview");
+                                String poster = jsonObject.getString("poster");
+                                Double rating = jsonObject.getDouble("rating");
+
+                                Movie movie = new Movie(title , poster , overview , rating);
+                                movieList.add(movie);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                             movieAdapter  = new MovieAdapter(MainActivity.this , movieList);
+
+                            recyclerView.setAdapter(movieAdapter);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(jsonArrayRequest);
+    }
+}
